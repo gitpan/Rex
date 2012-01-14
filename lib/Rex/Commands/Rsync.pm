dc42506630242024030c6e3a2590ea371740d8ff
Jan Gehring <cpan@imail.com>
2012-01-14 14:56:37 +0000

    Import of JFRIED/Rex-0.23.0 from CPAN.
    gitpan-cpan-distribution: Rex
    gitpan-cpan-version:      0.23.0
    gitpan-cpan-path:         JFRIED/Rex-0.23.0.tar.gz
    gitpan-cpan-author:       JFRIED
    gitpan-cpan-maturity:     released

--- lib/Rex/Commands/Rsync.pm
+++ lib/Rex/Commands/Rsync.pm
@@ -99,6 +99,13 @@ sub sync {
    if(Rex::Config->get_password_auth) {
       $cmd = sprintf($cmd, 'ssh');
       push(@expect_options, [
+                              qr{Are you sure you want to continue connecting},
+                              sub {
+                                 Rex::Logger::debug("Accepting key..");
+                                 my $fh = shift;
+                                 $fh->send("yes\n");
+                                 exp_continue;
+                              },
                               qr{password: $},
                               sub {
                                  Rex::Logger::debug("Want Password");
@@ -113,6 +120,13 @@ sub sync {
    else {
       $cmd = sprintf($cmd, 'ssh -i ' . Rex::Config->get_private_key);
       push(@expect_options, [
+                              qr{Are you sure you want to continue connecting},
+                              sub {
+                                 Rex::Logger::debug("Accepting key..");
+                                 my $fh = shift;
+                                 $fh->send("yes\n");
+                                 exp_continue;
+                              },
                               qr{Enter passphrase for key.*: $},
                               sub {
                                  Rex::Logger::debug("Want Passphrase");
