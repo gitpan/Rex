aa449e05466b04250c5a3bb8ae8c520219b22bc4
Jan Gehring <cpan@imail.com>
2013-09-23 21:27:00 +0000

    Import of JFRIED/Rex-0.43.3 from CPAN.
    gitpan-cpan-distribution: Rex
    gitpan-cpan-version:      0.43.3
    gitpan-cpan-path:         JFRIED/Rex-0.43.3.tar.gz
    gitpan-cpan-author:       JFRIED
    gitpan-cpan-maturity:     released

--- lib/Rex.pm
+++ lib/Rex.pm
@@ -88,7 +88,7 @@ our (@EXPORT,
       $MODULE_PATHS,
       $WITH_EXIT_STATUS);
 
-$VERSION = "0.43.2";
+$VERSION = "0.43.3";
 my $cur_dir;
 
 BEGIN {
@@ -567,6 +567,11 @@ sub import {
 
       }
 
+   }
+
+   if(exists $ENV{REX_REPORT_TYPE}) {
+      Rex::Logger::debug("Enabling reporting");
+      Rex::Config->set_do_reporting(1);
    }
 
    # we are always strict
